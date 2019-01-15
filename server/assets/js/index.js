bd47841075140a7ce367d6e794ab369c5bfe8639
nypinstripes <nypinstripes@gmail.com>
2019-01-15 18:05:47 -0500

    Fix scroll offset detection, various tidying.

--- server/assets/js/index.js
+++ server/assets/js/index.js
@@ -5,12 +5,14 @@
 // This value can be variable from one OS to another.
 
 const detectViewportOffset = () => {
+  document.body.style.height = 'calc(100vh + 2px)';
   let offsetWidth = window.innerWidth - document.body.clientWidth;
   let viewPortEl = document.getElementById('viewport');
   let viewPortHeight = `height=${window.innerHeight}, initial-scale=1`;
 
   document.body.setAttribute('data-offset', offsetWidth);
+  document.body.removeAttribute('style');
   viewPortEl.setAttribute('content', `width=device-width, ${viewPortHeight}`);
 };
 
-detectViewportOffset();
+window.onload = () => detectViewportOffset();
