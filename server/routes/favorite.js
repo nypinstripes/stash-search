9a821276c25599d2dfe33474312370a8a66500f7
nypinstripes <nypinstripes@gmail.com>
2019-02-06 18:08:57 -0500

    Remove unused server favorite route.

--- server/routes/favorite.js
+++
@@ -1,9 +0,0 @@
-const envVars = require('../config');
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res, next) => {
-  res.render('index', { envVars });
-});
-
-module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const favoriteRoutes = require('./routes/favorite');
 const indexRoutes = require('./routes/index');
 const path = require('path');
 const port = 3000;
@@ -8,7 +7,6 @@ const port = 3000;
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'pug');
 app.get('/', indexRoutes);
-app.use('/favorite', favoriteRoutes);
 app.use('/assets', express.static(__dirname + '/assets'));
 app.use('/', express.static(__dirname + '/../dist'));
 app.use('/*', indexRoutes);
